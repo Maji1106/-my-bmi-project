d37b48df536e1448260c64af1c22a7c6566cada6
maji1106 <nnnpphotmail.com@gmail.com>
2025-03-18 14:39:24 +0700

    HI

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/my-bmi-project",
+  plugins: [vue()],
+});
